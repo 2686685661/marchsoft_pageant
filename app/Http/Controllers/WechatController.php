09300069c98fe5960096dacd3ed736dfd8d9a5fd
331210877@qq.com <wq571314>
2018-03-20 19:08:05 +0800

    微信支付

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -23,6 +23,7 @@ class WechatController extends Controller
         $code=$request->get('code');
         $get_token_url='https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx2fffc402a50e03a5&secret=956397f1970f6d1b114a8ac835bc0a77&code='+$code+'&grant_type=authorization_code';
         $ch = curl_init();
+        dump($get_token_url);
         curl_setopt($ch,CURLOPT_URL,$get_token_url);
         curl_setopt($ch,CURLOPT_HEADER,0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
