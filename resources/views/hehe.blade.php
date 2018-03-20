95c736967ea183c2d9b9269b019b893884841fe0
331210877@qq.com <wq571314>
2018-03-20 22:32:06 +0800

    微信支付

--- resources/views/hehe.blade.php
+++ resources/views/hehe.blade.php
@@ -11,7 +11,7 @@
     <body>
         <button onClick="test();">支付宝</button>
         <button onClick="wxtest();">微信</button>
-        <p style="display:block;">{{ result }}</p>
+        <p style="display:block;">{{ $result }}</p>
     </body>
     <script>
         function test(){
