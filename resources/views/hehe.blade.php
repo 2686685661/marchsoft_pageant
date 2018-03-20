0627626586289338a9ff0be7fab31a9622828a28
331210877@qq.com <wq571314>
2018-03-20 22:21:04 +0800

    微信支付

--- resources/views/hehe.blade.php
+++ resources/views/hehe.blade.php
@@ -59,12 +59,8 @@
                 onBridgeReady();
              }
         }
-        function onBridgeReady(){
-            if (typeof(result) == "undefined") {
-                alert(result);
-            }else{
-                alert(result);
-            }
+        function onBridgeReady(){   
+            alert(result);   
             WeixinJSBridge.invoke(
                 'getBrandWCPayRequest', {
                     "appId":"wx2421b1c4370ec43b",     //公众号名称，由商户传入     
@@ -75,7 +71,6 @@
                     "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
                 },
                 function(res){     
-                    alert(res.err_msg);
                     if(res.err_msg == "get_brand_wcpay_request:ok" ) {
                         
                     }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
