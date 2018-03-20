37ecacfe8d5f419448cd2e896d76758f4dc9797d
331210877@qq.com <wq571314>
2018-03-20 17:53:06 +0800

    微信支付

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -10,19 +10,19 @@ use App\Models\orders; //订单表
 class WechatController extends Controller 
 {
     public function pay(Request $request){
-        // $config = [
-        //     'app_id'             => env('WECHAT_PAYMENT_APPID', 'wx2fffc402a50e03a5'),
-        //     'mch_id'             => env('WECHAT_PAYMENT_MCH_ID', '1439601702'),
-        //     'key'                => env('WECHAT_PAYMENT_KEY', 'def56bbd76f33932dbce862cd87d59de'),
-        //     'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'E:\Apache24\htdocs\marchsoft_pageant\public\path\to\apiclient_cert.pem'),    // XXX: 绝对路径！！！！
-        //     'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'E:\Apache24\htdocs\marchsoft_pageant\public\path\to\apiclient_key.pem'),      // XXX: 绝对路径！！！！
-        //     'notify_url'         => 'http://jk.mrwangqi.com/payments/wechatNotify',                           // 默认支付结果通知地址
-        // ];
+        $config = [
+            'app_id'             => env('WECHAT_PAYMENT_APPID', 'wx2fffc402a50e03a5'),
+            'mch_id'             => env('WECHAT_PAYMENT_MCH_ID', '1439601702'),
+            'key'                => env('WECHAT_PAYMENT_KEY', 'def56bbd76f33932dbce862cd87d59de'),
+            'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'E:\Apache24\htdocs\marchsoft_pageant\public\path\to\apiclient_cert.pem'),    // XXX: 绝对路径！！！！
+            'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'E:\Apache24\htdocs\marchsoft_pageant\public\path\to\apiclient_key.pem'),      // XXX: 绝对路径！！！！
+            'notify_url'         => 'http://jk.mrwangqi.com/payments/wechatNotify',                           // 默认支付结果通知地址
+        ];
         
-        // $app = Factory::payment($config);
-        dump($request->get('code'));
-        // $openid=$app->authCodeToOpenid($request->input['code']);
-        // dump($openid);
+        $app = Factory::payment($config);
+
+        $openid=$app->authCodeToOpenid($request->get('code'));
+        dump($openid);
         // $result = $app->order->unify([
         //     'body' => '腾讯充值中心-QQ会员充值',
         //     'out_trade_no' => '20150806125346',
