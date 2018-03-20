2f43bb9df57ba8adb078c416e4ec04264d08ca1d
331210877@qq.com <wq571314>
2018-03-20 17:39:47 +0800

    微信支付

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -20,19 +20,19 @@ class WechatController extends Controller
         ];
 
         $app = Factory::payment($config);
-        dump($request->input['code']);
-        $app->authCodeToOpenid($request->input['code']);
-
-        $result = $app->order->unify([
-            'body' => '腾讯充值中心-QQ会员充值',
-            'out_trade_no' => '20150806125346',
-            'total_fee' => 88,
-            'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
-            'notify_url' => 'https://pay.weixin.qq.com/wxpay/pay.action', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
-            'trade_type' => 'JSAPI',
-            'openid' => 'oUpF8uMuAJO_M2pxb1Q9zNjWeS6o',
-        ]);
-        dump($result);
+        // dump($request->input['code']);
+        $openid=$app->authCodeToOpenid($request->input['code']);
+        dump($openid);
+        // $result = $app->order->unify([
+        //     'body' => '腾讯充值中心-QQ会员充值',
+        //     'out_trade_no' => '20150806125346',
+        //     'total_fee' => 88,
+        //     'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
+        //     'notify_url' => 'https://pay.weixin.qq.com/wxpay/pay.action', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
+        //     'trade_type' => 'JSAPI',
+        //     'openid' => 'oUpF8uMuAJO_M2pxb1Q9zNjWeS6o',
+        // ]);
+        // dump($result);
     }
     public function wechatNotify(){
         echo "dasdasd";
