1f6094f9bb2db6c18865b1c976f75de3c1dc6cd0
331210877@qq.com <wq571314>
2018-03-20 19:43:12 +0800

    微信支付

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -32,13 +32,12 @@ class WechatController extends Controller
         curl_close($ch);
         $result = $app->order->unify([
             'body' => '助力三月',
-            'out_trade_no' => time(),
+            'out_trade_no' => '20150806125346',
             'total_fee' => 1,
             'notify_url' => 'https://pay.weixin.qq.com/wxpay/pay.action', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
             'trade_type' => 'JSAPI',
             'openid' => $openId->openid,
         ]);
-        $config=$app->configForJSSDKPayment($result['prepay_id']);
         dump($result['prepay_id']);
     }
     public function wechatNotify(){
