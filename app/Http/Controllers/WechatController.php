4376019b594bf7b361b7787033cb98b95a2cf03c
331210877@qq.com <wq571314>
2018-03-21 10:34:57 +0800

    微信支付

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -30,19 +30,20 @@ class WechatController extends Controller
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         $openid = curl_exec($ch);
         $openId=json_decode($openid);
-        session(['openId' => $openId->openid]);
-        curl_close($ch);
-        $result = $app->order->unify([
-            'body' => '助力三月',
-            'out_trade_no' => time(),
-            'total_fee' => 1,
-            'notify_url' => 'http://jk.mrwangqi.com/payments/wechatNotify', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
-            'trade_type' => 'JSAPI',
-            'openid' => session('openId'),
-        ]);
-        $paySign=$this->MakeSign($result);
-        $result['paySign']=$paySign;
-        return view('hehe',['result'=>$result]);
+        dump($openId);
+        // session(['openId' => $openId->openid]);
+        // curl_close($ch);
+        // $result = $app->order->unify([
+        //     'body' => '助力三月',
+        //     'out_trade_no' => time(),
+        //     'total_fee' => 1,
+        //     'notify_url' => 'http://jk.mrwangqi.com/payments/wechatNotify', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
+        //     'trade_type' => 'JSAPI',
+        //     'openid' => session('openId'),
+        // ]);
+        // $paySign=$this->MakeSign($result);
+        // $result['paySign']=$paySign;
+        // return view('hehe',['result'=>$result]);
     }
 
     /**
