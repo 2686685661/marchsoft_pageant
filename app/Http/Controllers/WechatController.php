bb35a35e8c7fd67e90361e2db4ccf07904e57ea5
331210877@qq.com <wq571314>
2018-03-23 09:42:41 +0800

    微信支付测试

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -50,7 +50,6 @@ class WechatController extends Controller
             $result = $app->order->unify([
                 'body' => '助力三月',
                 'out_trade_no' => time(),
-                // 'total_fee' => $total*100,
                 'total_fee' => 0.01,
                 'notify_url' => 'http://jk.mrwangqi.com/payments/wechatNotify', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
                 'trade_type' => 'JSAPI',
